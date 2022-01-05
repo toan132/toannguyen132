fc990df27812c018b0fbd9b3ad1399baf4113cda
toandev <toantv1302@gmail.com>
2022-01-06 02:58:59 +0700

    add jsb7.js file

---
+++ jsb7.js
@@ -0,0 +1,28 @@
+//bai1:
+// let a = prompt("First number?",1);
+// let b = prompt("Second number?",2);
+// alert(a + b); 
+// console.log(a+b)
+//bai2
+let txt ="nguyen van toan"
+console.log(txt.length) //length dem ky tu
+// lay so ky tu bao gom ca giau cach
+let trim ="      toan      "
+console.log(trim.trim()) //trim xoa bo 2 khoang cach dau tien va cuoi cung
+// // xoa bo 2 dau trang
+const indexof ="nguyen van toan"
+console.log(indexof.indexOf("toan")) //index0f tim vi tri tru
+// // tra ra vi tri
+let substring ="bai tap buoi 7"
+ketqua = substring.substring(4,8) // substring lay vi tri trong doan txt
+console.log(ketqua)
+// // lay chu tap trong doan txt
+
+let replace ="hello mindx" //replace thay the chu
+let newrreplace = replace.replace("mindx", "developer")
+console.log(newrreplace)
+// // thay the chu toan bo thanh chu 1 phan
+//bai 3
+
+let passtxt =prompt("sad boy")
+console.log(passtxt.length)
